fe2b2d69f3432532cb521233afd6ea8cf669b1b5
Saburo Okita <saburookita@Saburos-MacBook-Air.local>
2014-05-04 17:09:36 +0200

    Removed unnecessary method

--- TestHough/TestHough/main.cpp
+++ TestHough/TestHough/main.cpp
@@ -17,59 +17,7 @@ using namespace cv;
 using namespace std;
 
 static Point accumIndex(-1, -1);
-
 static void onMouse( int event, int x, int y, int, void * data );
-
-
-Vec3b HSVtoRGB( float h, float s, float v ) {
-    float r, g, b;
-	int i;
-	float f, p, q, t;
-	if( s == 0 ) {
-		// achromatic (grey)
-		r = g = b = v;
-		return Vec3b( b, g, r );
-	}
-	h /= 60;			// sector 0 to 5
-	i = floor( h );
-	f = h - i;			// factorial part of h
-	p = v * ( 1 - s );
-	q = v * ( 1 - s * f );
-	t = v * ( 1 - s * ( 1 - f ) );
-	switch( i ) {
-		case 0:
-			r = v;
-			g = t;
-			b = p;
-			break;
-		case 1:
-			r = q;
-			g = v;
-			b = p;
-			break;
-		case 2:
-			r = p;
-			g = v;
-			b = t;
-			break;
-		case 3:
-			r = p;
-			g = q;
-			b = v;
-			break;
-		case 4:
-			r = t;
-			g = p;
-			b = v;
-			break;
-		default:		// case 5:
-			r = v;
-			g = p;
-			b = q;
-			break;
-	}
-    return Vec3b( b, g, r );
-}
 
 int main(int argc, const char * argv[]) {
     namedWindow( "" );
